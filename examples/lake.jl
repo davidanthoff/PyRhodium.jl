9b610eae27ea30eef39b9051d20bf053e86c32db
davidanthoff <davidanthoff@users.noreply.github.com>
2023-12-27 20:26:29 +0000

    Format files using DocumentFormat

--- examples/lake.jl
+++ examples/lake.jl
@@ -4,16 +4,16 @@ using Distributions
 using DataFrames
 using Random
 
-function lake_problem(;pollution_limit = nothing,
-         b = 0.42,       # decay rate for P in lake (0.42 = irreversible)
-         q = 2.0,        # recycling exponent
-         μ = 0.02,       # mean of natural inflows
-         σ = 0.001,      # standard deviation of natural inflows
-         α = 0.4,        # utility from pollution
-         δ = 0.98,       # future utility discount rate
-         nsamples = 100) # monte carlo sampling of natural inflows)
+function lake_problem(; pollution_limit=nothing,
+    b=0.42,       # decay rate for P in lake (0.42 = irreversible)
+    q=2.0,        # recycling exponent
+    μ=0.02,       # mean of natural inflows
+    σ=0.001,      # standard deviation of natural inflows
+    α=0.4,        # utility from pollution
+    δ=0.98,       # future utility discount rate
+    nsamples=100) # monte carlo sampling of natural inflows)
 
-    Pcrit = fzero(x->x^q / (1 + x^q) - b * x, 0.01, 1.5)
+    Pcrit = fzero(x -> x^q / (1 + x^q) - b * x, 0.01, 1.5)
     nvars = length(pollution_limit)
     X = zeros(nvars)
     average_daily_P = zeros(nvars)
@@ -29,7 +29,7 @@ function lake_problem(;pollution_limit = nothing,
         rand!(d, natural_inflows)
 
         for t in 2:nvars
-            X[t] = (1 - b) * X[t - 1] + X[t - 1]^q / (1 + X[t - 1]^q) + pollution_limit[t] + natural_inflows[t]
+            X[t] = (1 - b) * X[t-1] + X[t-1]^q / (1 + X[t-1]^q) + pollution_limit[t] + natural_inflows[t]
             average_daily_P[t] += X[t] / nsamples
         end
 
@@ -37,7 +37,7 @@ function lake_problem(;pollution_limit = nothing,
     end
 
     max_P = maximum(average_daily_P)
-    utility = sum(α .* pollution_limit .* δ.^collect(1:nvars))
+    utility = sum(α .* pollution_limit .* δ .^ collect(1:nvars))
     intertia = sum(diff(pollution_limit) .> -0.02) / (nvars - 1)
 
     return max_P, utility, intertia, reliability
@@ -46,18 +46,18 @@ end
 m = Model(lake_problem)
 
 set_parameters!(m, [Parameter("pollution_limit"),
-                    Parameter("b"),
-                    Parameter("q"),
-                    Parameter("mean"),
-                    Parameter("stdev"),
-                    Parameter("delta")])
+    Parameter("b"),
+    Parameter("q"),
+    Parameter("mean"),
+    Parameter("stdev"),
+    Parameter("delta")])
 
 set_responses!(m, [Response("max_P", :MINIMIZE),
-                   Response("utility", :MAXIMIZE),
-                   Response("inertia", :MAXIMIZE),
-                   Response("reliability", :MAXIMIZE)])
+    Response("utility", :MAXIMIZE),
+    Response("inertia", :MAXIMIZE),
+    Response("reliability", :MAXIMIZE)])
 
-set_levers!(m, [RealLever("pollution_limit", 0.0, 0.1, length = 100)])
+set_levers!(m, [RealLever("pollution_limit", 0.0, 0.1, length=100)])
 
 # output = optimize(m, "NSGAII", 1000)
 output = optimize(m, "NSGAII", 100)
